3306855411e5b4a9278dddcc45fc8efc543b1023
Taganov Otabek <ataganov08@gmail.com>
2018-07-05 18:02:37 +0500

    first

---
+++ src/main/java/first_spring_html/config/MailConfig.java
@@ -0,0 +1,32 @@
+package first_spring_html.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+/**
+ * Created by Атабек on 30.06.2018.
+ */
+@Configuration
+public class MailConfig {
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("ataganov08@gmail.com");
+        mailSender.setPassword("fnfi1999ujl");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
